d5f7a1500068689b3f9a9d35523c39626ad89781
Srujana <srujanathota@live.com>
2016-12-14 22:34:12 -0800

    new commit

--- Post_Service_bids/js/main.js
+++ Post_Service_bids/js/main.js
@@ -1,19 +1,24 @@
 (function($) {
     
-    $(document).ready(function() {
-        
-        //dropdown Sub-menus
-		$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
-			event.preventDefault(); 
-			event.stopPropagation(); 
-			$(this).parent().siblings().removeClass('open');
-			$(this).parent().toggleClass('open');
-		});
+    $(document).ready(function() {        
+    
     // Multiple dropdown select
-    $(".chosen-select").chosen();
+    $("#langOptions").chosen();
+
+    //character count of textarea
+    var text_max = 500;
+    $('#char-count').html(text_max +' '+ 'Remaining');
+
+    $('#text-area').keyup(function() {
+        var text_length = $('#text-area').val().length;
+        var text_remaining = text_max - text_length;
+    
+    $('#char-count').html(text_remaining +'/' + text_max);
+    });
 
     //Range slider change of color on drag
     $('input[type="range"]').change(function () {
+        
         var val = ($(this).val() - $(this).attr('min')) / ($(this).attr('max') - $(this).attr('min'));
         $(this).css('background-image',
         '-webkit-gradient(linear, left top, right top, '
@@ -21,6 +26,23 @@
         + 'color-stop(' + val + ', #C5C5C5)'
         + ')'
         );
+    });
+    // Adding div in the job proposal page 
+    
+        var i = 3;
+    $(".add-div").click(function() {
+         // by default there are three divs in the page
+        var domElement = $(`<div class="milestone marginThirty">
+                        <div class="milestone-header">
+                            <span>Milestone`+" " +(++i)+`:</span>
+                            <span class="pull-right">
+                                <label>For</label>
+                                <input type="text" placeholder="$33.33"/>
+                            </span>
+                        </div>
+                        <div class="milestone-description" contentEditable="true" data-text="Describe a Task..."></div>
+                    </div>`);
+        $('#milestone-divs').append(domElement);
     });
     // Location search
     function initialize() {
